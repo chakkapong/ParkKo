54854e3b6236aadec6a7f5d00d658c350322b3f8
chakkapong <pikachu009@hotmail.com>
2014-10-22 15:17:42 +0700

    Add

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Park-Ko</title>
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+         <link href="styles/css/bootstrap.min.css" rel="stylesheet">
+         <link href="styles/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="">
+		<?php require_once('modules/ladypark/ladypark.php');?>
+		</div>
+	</body>
+</html>
---
+++ modules/ladypark/ladypark.php
@@ -0,0 +1,15 @@
+<div class="ladypark-contain">
+	<span class="title">ตั้งค่า LADY PARK</span>
+	<ul>
+		<li id="Block1" style="background-color:#1b9864;"></li>
+		<li id="Block2" style="background-color:#f5689c;">Lady Park</li>
+		<li id="Block3"></li>
+		<li id="Block4"></li>
+		<li id="Block5"></li>
+		<li id="Block6"></li>
+		<li id="Block7"></li>
+		<li id="Block8"></li>
+		<li id="Block9"></li>
+		<li id="Block10"></li>
+	</ul>
+</div>
